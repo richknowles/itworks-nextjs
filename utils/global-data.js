a2512b22e0cabe66d1c661456a3f66da69ffa56a
Rich Knowles <rightontron@protonmail.com>
2022-06-16 02:22:23 -0500

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,14 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'ℹtwerks.net'
-  '844-711-0007';
+    : 'ℹtwerks.net';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
     : 'Making I.T. Invisible';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : '©️2022 ℹ️.T. WORKS. All Rights Reserved';
+    : '©2022 ℹtwerks.net All Rights Reserved';
 
   return {
     name,
